12c290428cea7d77bc8eb8ea852f7f349be6eb3c
Anthony <you@example.comcls>
2018-10-18 13:04:45 +0000

    Created "Go!" button

--- app/src/main/java/com/example/oladapo/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/oladapo/braintrainer/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.oladapo.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startButton.setVisibility(View.INVISIBLE);
+            }
+        });
+
     }
 }
